260d8566b1bacc611bf5f3379bfe956073a3a802
BlueSillyDragon <BlueSillyDragon@outlook.com>
2024-08-29 15:15:20 -0600

    Added language header file

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include "inc/language.h"
 
 using namespace std;
 
 int main () {
 	
-	cout << "Shogi++" << endl;
+	switch (currentLanguage) {
+		case ENG:
+			cout << en_title << endl;
+			break;
+		case JPN:
+			cout << jp_title << endl;
+			break;
+	}
 	return 0;
 }
